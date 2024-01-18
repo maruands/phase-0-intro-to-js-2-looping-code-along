d4aa68980d4162d17407198fe74a6c06ec4baca2
maruands <maruands@gmail.com>
2024-01-18 12:28:51 +0300

    function to loop thru the array

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Code your solutions in this file
+// Code your solutions in this filef
+function writeCards(names) {
+
+    const newArray = [];
+    for (let i = 0; i < names.length; i++) {
+        const message = `Thank you, ${names[i]}, for the wonderful surprise gift!`;
+        newArray.push(message);
+    }
+
+    return newArray;
+}
+const users = ["Guadalupe", "Ollie", "Aki"];
+writeCards(users);
+
+
+function countDown(count) {
+    while(count >= 0){
+        console.log(count);
+        count --;
+    }
+}
